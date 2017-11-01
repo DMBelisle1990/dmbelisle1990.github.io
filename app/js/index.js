93cef1dbf47dc7b22d154ac9fd3e690b3bd4dcce
Daniel Belisle <daniel.belisle@aofl.com>
2017-10-31 18:32:16 -0700

    start work section

--- app/js/index.js
+++ app/js/index.js
@@ -1,5 +1,12 @@
 let content = document.getElementById('content');
 let work = document.getElementById('work');
+let temp = document.getElementById('temp');
 work.addEventListener('click', () => {
-  // content.style.
+  content.style.right = '100%';
+  temp.style.left = '10%';
+});
+
+temp.addEventListener('click', () => {
+  content.style.right = '0';
+  temp.style.left = '100%';
 });
