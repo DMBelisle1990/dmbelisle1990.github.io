c8944aaf611d7af1eed993fc9787e1754b6ac51e
Daniel Belisle <daniel.belisle@aofl.com>
2018-12-28 17:05:52 -0800

    Initial refactoring

--- js/index.js
+++ js/index.js
@@ -1,32 +1,32 @@
-var home = document.getElementById('home');
-var work = document.getElementById('work');
-var workContent = document.getElementById('work-content');
-var returnHome = document.getElementById('return-home');
-var modalOverlay = document.getElementById('modal-overlay');
-var modalToggles = document.getElementsByClassName('open-modal');
-var modalOpen = false;
+// var home = document.getElementById('home');
+// var work = document.getElementById('work');
+// var workContent = document.getElementById('work-content');
+// var returnHome = document.getElementById('return-home');
+// var modalOverlay = document.getElementById('modal-overlay');
+// var modalToggles = document.getElementsByClassName('open-modal');
+// var modalOpen = false;
 
-function toggleModal() {
-  modalOverlay.classList.toggle('visible');
-  if (modalOpen) {
+// function toggleModal() {
+//   modalOverlay.classList.toggle('visible');
+//   if (modalOpen) {
 
-  } else {
+//   } else {
 
-  }
-  modalOpen = !modalOpen;
-}
+//   }
+//   modalOpen = !modalOpen;
+// }
 
-Array.from(modalToggles).forEach(element => {
-  element.addEventListener('click', toggleModal)
-});
+// Array.from(modalToggles).forEach(element => {
+//   element.addEventListener('click', toggleModal)
+// });
 
-work.addEventListener('click', () => {
-  home.style.right = '100%';
-  workContent.style.left = '0%';
-});
+// work.addEventListener('click', () => {
+//   home.style.right = '100%';
+//   workContent.style.left = '0%';
+// });
 
-returnHome.addEventListener('click', () => {
-  home.style.right = '0';
-  workContent.style.left = '100%';
-});
+// returnHome.addEventListener('click', () => {
+//   home.style.right = '0';
+//   workContent.style.left = '100%';
+// });
 
