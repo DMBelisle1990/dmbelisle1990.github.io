b4761d3613578d837e9977d6bd333965c4f1ddbf
Daniel Belisle <daniel.belisle@aofl.com>
2019-02-03 10:10:15 -0800

    Text update

--- dist/danielbelisle.com.e31bb0bc.js
+++ dist/danielbelisle.com.e31bb0bc.js
@@ -260,12 +260,12 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 },{}],"index.js":[function(require,module,exports) {
 "use strict";
 
-require("./js/dock");
+require("./js/dock.js");
 
-require("./js/drag");
+require("./js/drag.js");
 
-require("./js/terminal");
-},{"./js/dock":"js/dock.js","./js/drag":"js/drag.js","./js/terminal":"js/terminal.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+require("./js/terminal.js");
+},{"./js/dock.js":"js/dock.js","./js/drag.js":"js/drag.js","./js/terminal.js":"js/terminal.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -292,7 +292,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65197" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54726" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
